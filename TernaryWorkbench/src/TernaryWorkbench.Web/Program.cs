68f70513d608c2dd732d37e1b4416577c330e40a
Steven Bos <steven@stevenbos.com>
2026-04-28 00:16:26 +0200

    broke the app when extracting documenation out of razor files into .md files and render them with markdown. Now fixed

--- TernaryWorkbench/src/TernaryWorkbench.Web/Program.cs
+++ TernaryWorkbench/src/TernaryWorkbench.Web/Program.cs
@@ -8,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
